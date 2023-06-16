19c3402389a43266603de7a9fd96ea50e671198b
leonardo-p <leonardo.phithak@gmail.com>
2023-06-16 12:28:07 +0100

    Creating +,-, and / operators using if-else statements

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const readline = require('readline-sync');
 
 console.log('Welcome to the calculator!');
+console.log('==========================');
 
-console.log('Please enter some input: ');
+console.log('Please enter the operator: ');
+const operator = readline.prompt();
+
+console.log('Please enter the first number: ');
 const response1 = readline.prompt();
 
-console.log('Please enter another input: ');
+console.log('Please enter the second number: ');
 const response2 = readline.prompt();
 
-console.log(+response1 * +response2);
+if (operator == '+') {
+    console.log('\The sum of the two numbers is ' + (+response1 + +response2));
+} else if (operator == '-') {
+    console.log('\The first number minus the second number is ' + (+response1 - +response2));
+} else if (operator == '*') {
+    console.log('\The product of the two numbers is ' + +response1 * +response2);
+} else if (operator == '/') {
+    console.log('\The first number divided by the second number is ' + +response1 / +response2);
+}
