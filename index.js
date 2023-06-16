40894bc727c49b5a52807e4ec6d5e1dd715287c5
leonardo-p <leonardo.phithak@gmail.com>
2023-06-16 12:37:43 +0100

    Creating +,- and / operators using switch statement

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
 console.log('==========================');
 
+// Asking for user prompt
 console.log('Please enter the operator: ');
 const operator = readline.prompt();
 
@@ -12,12 +13,29 @@ const response1 = readline.prompt();
 console.log('Please enter the second number: ');
 const response2 = readline.prompt();
 
-if (operator == '+') {
-    console.log('\The sum of the two numbers is ' + (+response1 + +response2));
-} else if (operator == '-') {
-    console.log('\The first number minus the second number is ' + (+response1 - +response2));
-} else if (operator == '*') {
-    console.log('\The product of the two numbers is ' + +response1 * +response2);
-} else if (operator == '/') {
-    console.log('\The first number divided by the second number is ' + +response1 / +response2);
+// Choosing between different operators using if-else statements
+// if (operator == '+') {
+//     console.log('\The sum of the two numbers is ' + (+response1 + +response2));
+// } else if (operator == '-') {
+//     console.log('\The first number minus the second number is ' + (+response1 - +response2));
+// } else if (operator == '*') {
+//     console.log('\The product of the two numbers is ' + +response1 * +response2);
+// } else if (operator == '/') {
+//     console.log('\The first number divided by the second number is ' + +response1 / +response2);
+// }
+
+// Choosing between different operators using switch statements
+switch (operator) {
+    case '+':
+        console.log('\The sum of the two numbers is ' + (+response1 + +response2));
+        break;
+    case '-':
+        console.log('\The first number minus the second number is ' + (+response1 - +response2));
+        break;
+    case '*':
+        console.log('\The product of the two numbers is ' + +response1 * +response2);
+        break;
+    case '/':
+        console.log('\The first number divided by the second number is ' + +response1 / +response2);
+        break;
 }
