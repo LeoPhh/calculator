f8b3fe12d3599764525dc1f702fce20197e2028c
leonardo-p <leonardo.phithak@gmail.com>
2023-06-16 12:19:54 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+
+console.log('Please enter some input: ');
+const response1 = readline.prompt();
+
+console.log('Please enter another input: ');
+const response2 = readline.prompt();
+
+console.log(+response1 * +response2);
