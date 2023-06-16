a4471b150f9d9d0a8b27890b6520485936643526
leonardo-p <leonardo.phithak@gmail.com>
2023-06-16 18:36:23 +0100

    Put everything in functions and made the program rerun after each calculation

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
-console.log('==========================');
+printWelcomeMessage();
 
-// Asking for user prompt
-console.log('Please enter the operator: ');
-const operator = readline.prompt();
-
-console.log('How many times would you like to ' + operator + '? ');
-const times = readline.prompt();
-
-// Collecting all the numbers from the user
-let numbers = [];
-for (let i = 1; i < (+times+1); i++) {
-    console.log('Enter number ' + i.toString() + ' : ');
-    const currentNumber = readline.prompt();
-    numbers.push(+currentNumber);
+var flag = true;
+while (flag) {
+    var operator = getOperator();
+    var times = getTimes();
+    var numbersArray = getNumbers();
+    calculateAndPrint();
+    flag = goAgain();
 }
 
-// Setting result equal to the first element of the array
-let result = numbers[0];
 
-// Calculating the corresponding result based on the operator
-for (let x = 1; x < numbers.length; x++) {
-    switch (operator) {
-        case '+':
-            result += numbers[x];
-            break;
-        case '-':
-            result -= numbers[x];
-            break;
-        case '*':
-            result = result * numbers[x];
-            break;
-        case '/':
-            result = result / numbers[x];
-            break;
+// Functions
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator!');
+    console.log('==========================');
+}
+
+function getOperator() {
+    console.log('Please enter the operator: ');
+    return readline.prompt();
+}
+
+function getTimes() {
+    console.log('How many times would you like to ' + operator + '? ');
+    const times = +readline.prompt();
+    if (isNaN(times)) {
+        return getTimes();
+    } else {
+        return times;
     }
 }
 
-// Printing the answer
-console.log('The answer is ' + result);
+function getNumbers() {
+    let numbers = [];
+    for (let i = 1; i < (+times+1); i++) {
+        console.log('Enter number ' + i.toString() + ' : ');
+        const currentNumber = readline.prompt();
+        numbers.push(+currentNumber);
+    }
+    return numbers
+}
+
+function calculateAndPrint() {
+    // Setting result equal to the first element of the array
+    let result = numbersArray[0];
+
+    // Calculating the corresponding result based on the operator
+    for (let x = 1; x < numbersArray.length; x++) {
+        if (operator == '+') {
+            result += numbersArray[x];
+        } else if (operator == '-') {
+            result -= numbersArray[x];
+        } else if (operator == '*') {
+            result = result * numbersArray[x];
+        } else if (operator == '/') {
+            result = result / numbersArray[x];
+        }
+    }
+
+    console.log('The answer is ' + result);
+}
+
+function goAgain() {
+    console.log("Would you like to do another calculation? [Y/N]")
+    return (readline.prompt() == 'Y');
+}
